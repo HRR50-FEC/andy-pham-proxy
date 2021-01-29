71b3851cd62bb234a3ec911a4fa9c45a7985b285
Andy Pham <apham3x@yahoo.com>
2021-01-28 19:51:54 -0500

    Ready to deploy

--- client/carousel/client/index.js
+++
@@ -1,6 +0,0 @@
-import App from './App.jsx';
-import React from 'react';
-import { render } from "react-dom";
-
-render(<App />, document.getElementById('carousel'))
-
--- client/carousel/database/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost/fecmodulec';
-
-const db = mongoose.connect(mongoUri, { useNewUrlParser: true });
-
-console.log('bzzz')
-
-module.exports = db;
